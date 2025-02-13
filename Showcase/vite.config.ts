3dc7dc27d5499f3a83f810c2bd6e742d67e40693
blackfenzer <6431346921@student.chula.ac.th>
2025-02-13 23:34:48 +0700

    vercel config

--- Showcase/vite.config.ts
+++ Showcase/vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-
 const ReactCompilerConfig = {
   /* ... */
 };
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(ReactCompilerConfig)],
+  build: {
+    outDir: "../dist", // Adjust output directory if needed
+  },
 });
