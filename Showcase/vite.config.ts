b4ee3d5f5195477e353d3e86faf7f340dc5df0cd
blackfenzer <6431346921@student.chula.ac.th>
2025-02-13 23:59:13 +0700

    update code

--- Showcase/vite.config.ts
+++ Showcase/vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-const ReactCompilerConfig = {
-  /* ... */
-};
+// import react from "@vitejs/plugin-react-swc";
+// const ReactCompilerConfig = {
+//   /* ... */
+// };
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(ReactCompilerConfig)],
+  // plugins: [react(ReactCompilerConfig)],
   build: {
     outDir: "dist", // Adjust output directory if needed
   },
